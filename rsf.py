faa842f58855308fa28844bca1481dc3e6657195
John Leeman <kd5wxb@gmail.com>
2015-05-01 16:55:13 -0400

    Clean unused

--- rsf.py
+++ rsf.py
@@ -6,6 +6,7 @@ from collections import namedtuple
 
 class RateState(object):
     def __init__(self):
+        # Rate and state model parameters
         self.mu0 = 0
         self.a = 0
         self.b = 0
@@ -13,17 +14,23 @@ class RateState(object):
         self.k = 0
         self.v = 0
         self.vlp = 0
-        self.model_time = []
-        self.dispHist = []
-        self.results = namedtuple("results",["time","displacement","velocity","friction","state1","state2"])
+        self.model_time = [] # List of times we want answers at
+        # Results of running the model
+        self._dispHist = []
+        self.results = namedtuple("results",["time","displacement","velocity","friction","state1"])
         self.results.time = []
         self.results.displacement = []
         self.results.velocity = []
         self.results.friction = []
         self.results.state1 = []
-        self.results.state2 = []
+        # Integrator settings
+        self.abserr = 1.0e-12
+        self.relerr = 1.0e-12
 
     def _integrationStep(self, w, t, p):
+        """
+        Do the calculation for a time-step
+        """
         mu, theta, self.v = w
         mu0, vlp, a, b, dc, k = p
         self.v = self.v * exp((mu - mu0 - b * log(self.v * theta / dc)) / a)
@@ -35,28 +42,24 @@ class RateState(object):
         return [dmu_dt,dtheta_dt]
 
     def solve(self):
-
-        # Parameters from RSF model
+        """
+        Runs the integrator to actually solve the model and returns a
+        named tuple of results.
+        """
+        # Parameters for the model
         p = [self.mu0,self.vlp,self.a,self.b,self.dc,self.k]
 
         # Initial conditions at t = 0
-        # mu = reference friction value
-        # theta = dc/v
-        # velocity = v
+        # mu = reference friction value, theta = dc/v, velocity = v
         w0 = [self.mu0,self.dc/self.v,self.v]
-
-        # Integrator settings
-        abserr = 1.0e-12
-        relerr = 1.0e-12
 
         # Append initial value to velocity history
         self._vHist = [self.v]
 
         # Solve it
         wsol = integrate.odeint(self._integrationStep, w0, self.model_time, args=(p,),
-                                atol=abserr, rtol=relerr)
+                                atol=self.abserr, rtol=self.relerr)
 
-        #return SimResults(velocity=self._vHist, time=wsol[:,0], friction=wsol[:,1])
         self.results.friction = wsol[:,0]
         self.results.state1 = wsol[:,1]
         self.results.velocity = np.array(self._vHist)
