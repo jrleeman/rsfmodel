Fix velocity of slider calculation with vref instead of v dynamic

rsf.py
@@ -14,15 +14,10 @@ class RateState(object):
         self.k = 0
         self.v = 0
         self.vlp = 0
+        self.vref = 1.
         self.model_time = [] # List of times we want answers at
         # Results of running the model
-        self._dispHist = []
-        self.results = namedtuple("results",["time","displacement","velocity","friction","state1"])
-        self.results.time = []
-        self.results.displacement = []
-        self.results.velocity = []
-        self.results.friction = []
-        self.results.state1 = []
+        self.results = namedtuple("results",["time","displacement","slider_velocity","friction","state1"])
         # Integrator settings
         self.abserr = 1.0e-12
         self.relerr = 1.0e-12
@@ -33,8 +28,7 @@ class RateState(object):
         """
         mu, theta, self.v = w
         mu0, vlp, a, b, dc, k = p
-        self.v = self.v * exp((mu - mu0 - b * log(self.v * theta / dc)) / a)
-        self._vHist.append(self.v)
+        self.v = self.vref * exp((mu - mu0 - b * log(self.vref * theta / dc)) / a)
 
         dmu_dt = k * (vlp - self.v)
         dtheta_dt = 1. - self.v * theta / dc
@@ -53,15 +47,12 @@ class RateState(object):
         # mu = reference friction value, theta = dc/v, velocity = v
         w0 = [self.mu0,self.dc/self.v,self.v]
 
-        # Append initial value to velocity history
-        self._vHist = [self.v]
-
         # Solve it
         wsol = integrate.odeint(self._integrationStep, w0, self.model_time, args=(p,),
                                 atol=self.abserr, rtol=self.relerr)
 
         self.results.friction = wsol[:,0]
         self.results.state1 = wsol[:,1]
-        self.results.velocity = np.array(self._vHist)
+        self.results.slider_velocity = self.vref * np.exp((self.results.friction - self.mu0 - self.b * np.log(self.vref * self.results.state1 / self.dc)) / self.a)
 
         return self.results

run_example.py
@@ -19,7 +19,7 @@ model.model_time = np.arange(0,30.01,0.01)
 
 results = model.solve()
 
-velocity = results.velocity
+velocity = results.slider_velocity
 friction = results.friction
 state = results.state1
 
@@ -34,7 +34,7 @@ t = np.arange(0,30,0.01)
 cjm_disp, cjm_mu = np.loadtxt('cjm_model_comparison/cjm_model.dis',skiprows=2,unpack=True)
 cjm_time, cjm_mu = np.loadtxt('cjm_model_comparison/cjm_model.tim',skiprows=2,unpack=True)
 cjm_state = np.loadtxt('cjm_model_comparison/cjm_model.sta',skiprows=2,unpack=True,usecols=[1])
-cjm_vel = np.loadtxt('cjm_model_comparison/cjm_model.vel',skiprows=2,unpack=True,usecols=[0])
+cjm_vel,cjm_mu2 = np.loadtxt('cjm_model_comparison/cjm_model.vel',skiprows=2,unpack=True)
 cjm_vel = np.exp(cjm_vel)*1.
 
 
@@ -61,5 +61,11 @@ fig = plt.figure(3)
 plt.title('State Variable')
 plt.plot(model.model_time,state,color='k',label='JRL')
 plt.plot(cjm_time,cjm_state,color='r',label='CJM')
+
+fig = plt.figure(4)
+plt.title('Phase')
+#ln(v/v0),mu
+plt.plot(np.log(model.results.slider_velocity/model.vref),model.results.friction,color='k',label='JRL')
+plt.plot(np.log(cjm_vel/1.),cjm_mu2,color='r',label='CJM')
 
 plt.show()