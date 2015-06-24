713c6fb5fc0f47fa342700774a0ffd7e297b8af0
John Leeman <kd5wxb@gmail.com>
2015-06-24 17:01:05 -0400

    Modify remaining two examples

--- examples/defining_new_state_relation.py
+++ examples/defining_new_state_relation.py
@@ -28,7 +28,7 @@ model.k = 1e-3  # Normalized System stiffness (friction/micron)
 model.v = 1.  # Initial slider velocity, generally is vlp(t=0)
 model.vref = 1.  # Reference velocity, generally vlp(t=0)
 
-state1 = MyStateRelation(model)
+state1 = MyStateRelation()
 state1.b = 0.005  # Empirical coefficient for the evolution effect
 state1.Dc = 10.  # Critical slip distance
 
